004e2e3e6b52608f5992b8ac0a035a4ab41105ff
xby1024 <xuebing.yan@qq.com>
2021-04-10 00:38:51 +0800

    add an error

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -21,3 +21,7 @@ def test_sample_multiple_words():
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
     assert len(phrase.split()) >= 5
+
+
+def test_failure():
+    assert 0 == 1
